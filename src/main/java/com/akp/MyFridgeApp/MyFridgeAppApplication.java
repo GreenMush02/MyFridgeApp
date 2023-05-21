2a7be811d7a42d3f520b964fbef0fd084624e7f8
GreenMush02 <domino280702@gmail.com>
2023-05-21 17:32:11 +0200

    Initial commit

---
+++ src/main/java/com/akp/MyFridgeApp/MyFridgeAppApplication.java
@@ -0,0 +1,13 @@
+package com.akp.MyFridgeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFridgeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFridgeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akp/MyFridgeApp/MyFridgeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akp.MyFridgeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFridgeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
